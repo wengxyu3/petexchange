caff530558cd3f57672bde08e38c43ff42329db0
joshu <joshu@LAPTOP-07JNIU1S>
2020-03-07 19:35:17 -0500

    Optimized refreshScene

--- src/BulletinPane.java
+++ src/BulletinPane.java
@@ -3,37 +3,36 @@ package src;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 
-import javax.swing.JScrollBar;
-
 import backEnd.Account;
 import backEnd.Post;
+import backEnd.bulletinboard;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
-import backEnd.bulletinboard;
 
 public class BulletinPane extends BorderPane {
 	// TODO replace this with BulletinBoard
 	// ArrayList<Post> posts;
-	ArrayList<Post> posts = new ArrayList<Post>();
+	ArrayList<Post> posts = new ArrayList<>();
 	GridPane postPane;
-	ArrayList<PostHUD> postHUDs = new ArrayList<PostHUD>();
+	ArrayList<PostHUD> postHUDs = new ArrayList<>();
 	String username;
 	Label userlabel;
 	ScrollPane postScrollPane = new ScrollPane();
+
 	BulletinPane(Account user, String file) {
 		username = user.getUsername();
-		userlabel = new Label("Logged in as: "+username);
+		userlabel = new Label("Logged in as: " + username);
 		bulletinboard bb = new bulletinboard(file);
 		// TODO replace with bullitenBoard stuff
 		postPane = new GridPane();
-		
+
 //		ScrollPane postScrollPane = new ScrollPane();
 		postScrollPane.setContent(postPane);
-		
+
 		TextField textAddField = new TextField();
 		textAddField.setOnKeyPressed(e -> {
 			if (e.getCode() == KeyCode.ENTER && !textAddField.getText().equals("")) {
@@ -49,16 +48,17 @@ public class BulletinPane extends BorderPane {
 		this.setBottom(textAddField);
 		refreshScene(bb);
 	}
+
 	BulletinPane(String user, String file) {
 		username = user;
-		userlabel = new Label("Logged in as: "+username);
+		userlabel = new Label("Logged in as: " + username);
 		bulletinboard bb = new bulletinboard(file);
 		// TODO replace with bullitenBoard stuff
 		postPane = new GridPane();
-		
+
 //		ScrollPane postScrollPane = new ScrollPane();
 		postScrollPane.setContent(postPane);
-		
+
 		TextField textAddField = new TextField();
 		textAddField.setOnKeyPressed(e -> {
 			if (e.getCode() == KeyCode.ENTER && !textAddField.getText().equals("")) {
@@ -74,25 +74,28 @@ public class BulletinPane extends BorderPane {
 		this.setBottom(textAddField);
 		refreshScene(bb);
 	}
+
 	void createPost(Post input, bulletinboard bb) {
 		// creates new postHUD with displayIndex of the last postHUD
 		bb.addPost(input);
 		refreshScene(bb);
 	}
-	void removePost(Post input, bulletinboard bb) {
-		bb.deletePost(input, username);
-		refreshScene(bb);
-	}
+
 	void refreshScene(bulletinboard bb) {
 		bb.saveBulletin();
 		postHUDs.clear();
 		postPane.getChildren().clear();
-		for(int i=0; i<bb.listPosts().size(); i++) {
-			postHUDs.add(new PostHUD(bb.listPosts().get(i), username));
+		for (int i = 0; i < bb.listPosts().size(); i++) {
+			PostHUD postHUD = new PostHUD(bb.listPosts().get(i), username);
+			postHUD.deleteButton.setOnAction(e -> removePost(postHUD.post, bb));
+			postHUDs.add(postHUD);
+			postPane.add(postHUD, 0, i);
 		}
-		for(int i=0; i<postHUDs.size(); i++) {
-			postPane.add(postHUDs.get(i), 0, i);
-		}
+	}
+
+	void removePost(Post input, bulletinboard bb) {
+		bb.deletePost(input, username);
+		refreshScene(bb);
 	}
 
 }
